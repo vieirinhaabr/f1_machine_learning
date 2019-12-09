e00c7f606d2cdd8bbf049798f0d22e911ae01998
Wandreus Caetano Vieira <wandreusvieira@gmail.com>
2019-12-08 23:19:34 -0300

    get temp

--- get_csv-f1tv_json.py
+++ get_csv-f1tv_json.py
@@ -26,33 +26,60 @@ class extract_as_csv(object):
 
             return path_db
 
-        def track_temp():
-            j_temp = json_file['Weather']
-            j_temp = j_temp['graph']
-            j_temp = j_temp['data']
-            j_temp = j_temp['pTrack']
-
+        def for_loop_by_time(json):
             Time = []
-            Temp = []
+            Something = []
 
             i = 0
-            for value in j_temp:
+            for value in json:
                 if i == 0:
                     Time.append(value)
                     i = 1
                 else:
-                    Temp.append(value)
+                    Something.append(value)
                     i = 0
 
-            Track_Temp = {"Time": Time, "Temperature": Temp}
+            return Time, Something
 
-            Track_Temp_Data = pd.DataFrame(data=Track_Temp)
-            Track_Temp_Data = Track_Temp_Data.set_index('Time')
+        def weather():
+            j_temp = json_file['Weather']
+            j_temp = j_temp['graph']
+            j_temp = j_temp['data']
 
-            path_db = generate_csv_name('Track_Temp')
+            def temperature():
+                def temp_df(j_temp):
+                    Time, Temp = for_loop_by_time(j_temp)
 
-            Track_Temp_Data.to_csv(path_db)
+                    Track_Temp = {"Time": Time, "Temperature": Temp}
 
-        track_temp()
+                    Track_Temp_Data = pd.DataFrame(data=Track_Temp)
+                    Track_Temp_Data = Track_Temp_Data.set_index('Time')
+
+                    return Track_Temp_Data
+
+                def track_temp(j_temp):
+                    j_temp = j_temp['pTrack']
+
+                    Track_Temp_Data = temp_df(j_temp)
+
+                    path_db = generate_csv_name('Track_Temp')
+
+                    Track_Temp_Data.to_csv(path_db)
+
+                def air_temp(j_temp):
+                    j_temp = j_temp['pAir']
+
+                    Track_Temp_Data = temp_df(j_temp)
+
+                    path_db = generate_csv_name('Air_Temp')
+
+                    Track_Temp_Data.to_csv(path_db)
+
+                track_temp(j_temp)
+                air_temp(j_temp)
+
+            temperature()
+
+        weather()
 
     extract_f1_json('https://livetiming.formula1.com/static/2019/2019-12-01_Abu_Dhabi_Grand_Prix/2019-12-01_Race/SPFeed.json')
